d72c58164058b39ff379a334712070a91d40b3cc
santa <santariver@yandex.ru>
2023-04-12 10:39:49 +0500

    asdasd

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ async function request()
     console.log(fetch("http://jsonplaceholder.typicode.com/posts"));
     let response = await fetch("http://jsonplaceholder.typicode.com/posts");
     console.log('response.json()', response.json())
+    console.log('response');
     console.log(response);
     if (response.ok) {
         let json = await response.json()
